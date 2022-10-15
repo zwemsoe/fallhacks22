5bd71c690747869b967a1776ba4561213fdad9c5
zsoe <zwemsoe@gmail.com>
2022-10-15 14:20:41 -0700

    cleanup

---
+++ backend/game-handler.js
@@ -0,0 +1,166 @@
+require("dotenv").config();
+const Ably = require("ably");
+
+const ABLY_API_KEY = process.env.ABLY_API_KEY;
+
+const GAME_ROOM_CAPACITY = 3;
+const CANVAS_HEIGHT = 700;
+const CANVAS_WIDTH = 700;
+const SHIP_PLATFORM = CANVAS_HEIGHT - 12;
+const BULLET_PLATFORM = SHIP_PLATFORM - 32;
+const PLAYER_VERTICAL_INCREMENT = 20;
+const PLAYER_VERTICAL_MOVEMENT_UPDATE_INTERVAL = 1000;
+const PLAYER_SCORE_INCREMENT = 5;
+const P2_WORLD_TIME_STEP = 1 / 16;
+const MIN_PLAYERS_TO_START_GAME = 3;
+const GAME_TICKER_MS = 100;
+
+let players = {};
+let playerChannels = {};
+let gameOn = false;
+let alivePlayers = 0;
+let totalPlayers = 0;
+let gameRoomName = "primary";
+let gameRoom;
+let gameTickerOn = false;
+
+// instantiate to Ably
+const realtime = new Ably.Realtime({
+  key: ABLY_API_KEY,
+  echoMessages: false,
+});
+
+// wait until connection with Ably is established
+realtime.connection.once("connected", () => {
+  gameRoom = realtime.channels.get(gameRoomName);
+
+  // subscribe to new players entering the game
+  gameRoom.presence.subscribe("enter", (player) => {
+    console.log("new player");
+    let newPlayerId;
+    alivePlayers++;
+    totalPlayers++;
+    newPlayerId = player.clientId;
+    playerChannels[newPlayerId] = realtime.channels.get(
+      "clientChannel-" + player.clientId
+    );
+    if (totalPlayers == 1) {
+      gameTickerOn = true;
+      startGameDataTicker();
+    }
+    newPlayerObject = {
+      id: newPlayerId,
+      x: playerXposition(colorIndex),
+      y: playerYposition(colorIndex),
+      score: 0,
+      nickname: player.data.nickname,
+      isAlive: true,
+    };
+    players[newPlayerId] = newPlayerObject;
+    subscribeToPlayerInput(playerChannels[newPlayerId], newPlayerId);
+  });
+
+  // subscribe to players leaving the game
+  gameRoom.presence.subscribe("leave", (player) => {
+    let leavingPlayer = player.clientId;
+    alivePlayers--;
+    totalPlayers--;
+    delete players[leavingPlayer];
+  });
+  gameRoom.publish("thread-ready", {
+    start: true,
+  });
+});
+
+// start the game tick
+function startGameDataTicker() {
+  let tickInterval = setInterval(() => {
+    if (!gameTickerOn) {
+      clearInterval(tickInterval);
+    } else {
+      // fan out the latest game state
+      gameRoom.publish("game-state", {
+        // players: players,
+        // playerCount: totalPlayers,
+        // shipBody: copyOfShipBody.position,
+        // bulletOrBlank: bulletOrBlank,
+        // gameOn: gameOn,
+        // killerBullet: killerBulletId,
+      });
+    }
+  }, GAME_TICKER_MS);
+}
+
+// subscribe to each player's input events
+function subscribeToPlayerInput(channelInstance, playerId) {
+  channelInstance.subscribe("pos", (msg) => {
+    if (msg.data.keyPressed == "left") {
+      if (players[playerId].x - 20 < 20) {
+        players[playerId].x = 25;
+      } else {
+        players[playerId].x -= 20;
+      }
+    } else if (msg.data.keyPressed == "right") {
+      if (players[playerId].x + 20 > 1380) {
+        players[playerId].x = 1375;
+      } else {
+        players[playerId].x += 20;
+      }
+    }
+  });
+  channelInstance.subscribe("start-game", (msg) => {});
+
+  // subscribe to players being shot
+  channelInstance.subscribe("dead-notif", (msg) => {});
+}
+
+// finish the game
+function finishGame(playerId) {
+  console.log("finished");
+
+  gameRoom.publish("game-over", {
+    winner: winnerName,
+  });
+}
+
+function playerXposition(index) {
+  switch (index) {
+    case 0:
+      return between(22, 246);
+    case 1:
+      return between(247, 474);
+    case 2:
+      return between(475, 702);
+    case 3:
+      return between(703, 930);
+    case 4:
+      return between(931, 1158);
+    case 5:
+      return between(1159, 1378);
+    default:
+      return between(22, 246);
+  }
+}
+
+function playerYposition(index) {
+  switch (index) {
+    case 0:
+      return between(22, 246);
+    case 1:
+      return between(247, 474);
+    case 2:
+      return between(475, 702);
+    case 3:
+      return between(703, 930);
+    case 4:
+      return between(931, 1158);
+    case 5:
+      return between(1159, 1378);
+    default:
+      return between(22, 246);
+  }
+}
+
+function between(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,14 @@
 require("dotenv").config();
-
-const {
-  Worker,
-  isMainThread,
-  parentPort,
-  workerData,
-  threadId,
-  MessageChannel,
-} = require("worker_threads");
-
-const { nanoid } = require("nanoid");
-
+const { Worker, isMainThread, threadId } = require("worker_threads");
 const express = require("express");
 const Ably = require("ably");
-const p2 = require("p2");
 const app = express();
 const { ABLY_API_KEY, PORT } = process.env;
-
-const globalGameName = "main-game-thread";
-const GAME_ROOM_CAPACITY = 6;
-let globalChannel;
-let activeGameRooms = {};
 
 // instantiate to Ably
 const realtime = new Ably.Realtime({
   key: ABLY_API_KEY,
   echoMessages: false,
-});
-
-app.get("/auth", (request, response) => {
-  const tokenParams = { clientId: nanoid() };
-  realtime.auth.createTokenRequest(tokenParams, function (err, tokenRequest) {
-    if (err) {
-      response
-        .status(500)
-        .send("Error requesting token: " + JSON.stringify(err));
-    } else {
-      response.setHeader("Content-Type", "application/json");
-      response.send(JSON.stringify(tokenRequest));
-    }
-  });
 });
 
 app.get("/", (request, response) => {
@@ -48,90 +17,24 @@ app.get("/", (request, response) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  response.sendFile(__dirname + "/views/intro.html");
-});
-
-app.get("/gameplay", (request, response) => {
-  let requestedRoom = request.query.roomCode;
-  let isReqHost = request.query.isHost == "true";
-  if (!isReqHost && activeGameRooms[requestedRoom]) {
-    if (
-      activeGameRooms[requestedRoom].totalPlayers + 1 <= GAME_ROOM_CAPACITY &&
-      !activeGameRooms[requestedRoom].gameOn
-    ) {
-      response.sendFile(__dirname + "/views/index.html");
-    } else {
-      console.log("here");
-      response.sendFile(__dirname + "/views/gameRoomFull.html");
-    }
-  } else if (isReqHost) {
-    response.sendFile(__dirname + "/views/index.html");
-  } else {
-    response.sendFile(__dirname + "/views/gameRoomFull.html");
-  }
-  console.log(JSON.stringify(activeGameRooms));
-});
-
-app.get("/winner", (request, response) => {
-  response.sendFile(__dirname + "/views/winner.html");
-});
-
-app.get("/gameover", (request, response) => {
-  response.sendFile(__dirname + "/views/gameover.html");
 });
 
 const listener = app.listen(PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-// wait until connection with Ably is established
-realtime.connection.once("connected", () => {
-  globalChannel = realtime.channels.get(globalGameName);
-  // subscribe to new players entering the game
-  globalChannel.presence.subscribe("enter", (player) => {
-    generateNewGameThread(
-      player.data.isHost,
-      player.data.nickname,
-      player.data.roomCode,
-      player.clientId
-    );
-  });
-});
+require("./game-handler");
 
-function generateNewGameThread(
-  isHost,
-  hostNickname,
-  hostRoomCode,
-  hostClientId
-) {
-  if (isHost && isMainThread) {
-    const worker = new Worker("./server-worker.js", {
-      workerData: {
-        hostNickname: hostNickname,
-        hostRoomCode: hostRoomCode,
-        hostClientId: hostClientId,
-      },
-    });
-    console.log(`CREATING NEW THREAD WITH ID ${threadId}`);
-    worker.on("error", (error) => {
-      console.log(`WORKER EXITED DUE TO AN ERROR ${error}`);
-    });
-    worker.on("message", (msg) => {
-      if (msg.roomName && !msg.resetEntry) {
-        activeGameRooms[msg.roomName] = {
-          roomName: msg.roomName,
-          totalPlayers: msg.totalPlayers,
-          gameOn: msg.gameOn,
-        };
-      } else if (msg.roomName && msg.resetEntry) {
-        delete activeGameRooms[msg.roomName];
-      }
-    });
-    worker.on("exit", (code) => {
-      console.log(`WORKER EXITED WITH THREAD ID ${threadId}`);
-      if (code !== 0) {
-        console.log(`WORKER EXITED DUE TO AN ERROR WITH CODE ${code}`);
-      }
-    });
-  }
-}
+// app.get("/auth", (request, response) => {
+//   const tokenParams = { clientId: nanoid() };
+//   realtime.auth.createTokenRequest(tokenParams, function (err, tokenRequest) {
+//     if (err) {
+//       response
+//         .status(500)
+//         .send("Error requesting token: " + JSON.stringify(err));
+//     } else {
+//       response.setHeader("Content-Type", "application/json");
+//       response.send(JSON.stringify(tokenRequest));
+//     }
+//   });
+// });
